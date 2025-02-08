800b51517996f6dca1de00a1fc50ff0e9f60f25a
Lexminimi <44340610+Lexminimi@users.noreply.github.com>
2025-02-08 17:50:37 +0100

    new deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
-// vue.config.js file to be placed in the root of your repository
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/hopsfront/' : '/',
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/hopsfront/'
+    : '/'
+}
