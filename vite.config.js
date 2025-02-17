e2cc33efdba6bc6f84f4324afa79f9f2646bb584
Lexminimi <44340610+Lexminimi@users.noreply.github.com>
2025-02-17 12:00:04 +0100

    small adjustment

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/hopsfront/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
