517a3441fc1f91420825342b626e9bbed0191f98
Lexminimi <fischerbalazs2@gmail.com>
2025-03-16 11:43:16 +0100

    Revert "chnaged base url and build base"
    This reverts commit 1ec94e50559dc171b2c48047a3e23f4d3d56c9fd.

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/hopsfront/',
+  base: '/',
   plugins: [
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      injectRegister: 'script',
-      strategies: 'injectManifest',
-      srcDir: 'src',
+      injectRegister: 'auto',
+      strategies: 'generateSW',
       filename: 'sw.js',
-      base: '/hopsfront/',
-      buildBase: '/hopsfront/',
+      manifestFilename: 'manifest.json',
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
         name: 'My Awesome App',
@@ -26,12 +24,12 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: '/hopsfront/img/icons/icon-192x192.png',
+            src: 'img/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/hopsfront/img/icons/icon-512x512.png',
+            src: 'img/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
@@ -39,7 +37,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        navigateFallback: '/hopsfront/index.html',
+        navigateFallback: 'index.html',
         cleanupOutdatedCaches: true,
         sourcemap: true,
         runtimeCaching: [
